da9c0a65b87d8bc95a1b4eecfc8ec7b283cfc8cd
Jazon Coelho <falecomjazon@gmail.com>
2023-01-09 21:49:24 -0300

    conclusão da aula 29 - exclusão

--- src/componentes/pages/Projetos.js
+++ src/componentes/pages/Projetos.js
@@ -7,15 +7,61 @@ import { useLocation } from "react-router-dom"
 import styles from "./Projetos.module.css"
 import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
+import { useState , useEffect, useRef} from "react"
+import userEvent from "@testing-library/user-event"
+import ProjetoCard from "../projeto/ProjetoCard"
+import Loading from "../layout/Loading"
 function Projetos() {
-
+    const [projetos,setProjetos] = useState([])
     const location = useLocation()
+    const [removeLoading, setRemoveLoading] = useState(false)
+    const [projetoMessage,setProjetoMessage] = useState('')
+
+    useEffect(()=>{
+        setTimeout(()=>{
+
+       
+
+        fetch('http://localhost:5000/projetos',{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+            },
+        })
+        .then((resp)=>resp.json())
+        .then((data)=>{
+           // console.log(data)
+            setProjetos(data)
+            setRemoveLoading(true)
+        })
+        .catch((err)=>console.log(err))
+    },300)
+    },[])
+
     let message = ''
 
     //console.log(location.state)
     if(location.state){
         message = location.state.message
     }
+
+    function removerProjeto(id) {
+        fetch(`http://localhost:5000/projetos/${id}`,{
+            method:'DELETE',
+            headers:{
+                'Content-Type':'application/json',
+            },
+        })
+        .then((resp)=>resp.json())
+        .then((data)=>{
+           // console.log(data)
+            setProjetos(projetos.filter((projeto) =>projeto.id !== id))
+            setProjetoMessage('Projeto excluído com sucesso')
+        })
+        .catch((err)=>console.log(err)) 
+        
+    }
+
     return (
         <div className={styles.project_container}>
             <NavBar/> 
@@ -24,13 +70,30 @@ function Projetos() {
                 <LinkButton to="/novoprojeto" text="Novo Projeto"/>
                 
             </div>
+            {message && <Message msg={message} type="success"/>}
+            {projetoMessage && <Message msg={projetoMessage} type="success"/>}
           {/*   <section className={styles.home_container}>
                 <h1>Meus projetos</h1>
                 {message && <Message msg={message} type="success"/>}
                 
             </section> */}
             <Container customClass="start">
-                <p>Projetos...</p>
+               
+                {
+                    projetos.map((projeto)=>{
+                       return <ProjetoCard 
+                       id={projeto.id}
+                       name={projeto.nomeprojeto}
+                       orcamento={projeto.orcamento}
+                       category={projeto.category.name}
+                       key={projeto.id}
+                       handleRemove={removerProjeto}
+                       />
+                })}
+            {!removeLoading && <Loading/>}
+            {removeLoading && projetos.length ===0 &&(
+                <p>Não há projetos cadastrados</p>
+            )}
             </Container>
               <Footer />  
         </div>
---
+++ src/componentes/projeto/ProjetoCard.js
@@ -0,0 +1,30 @@
+import { BsPencil, BsFillTrashFill} from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+import styles from './ProjetoCard.module.css'
+function ProjetoCard({id, name, orcamento, category, handleRemove}) {
+    const remove = (e)=>{
+        e.preventDefault()
+        handleRemove(id)
+    }
+    
+    return (
+        <div className={styles.projeto_card}>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento:</span>R$ {orcamento}
+            </p>
+            <p className={styles.category_text}>
+                <span className={`${styles[category.toLowerCase()]}`}>{category}</span>
+            </p>
+            <div className={styles.projeto_card_actions}>
+                <Link to="/">
+                    <BsPencil />Editar
+                </Link>
+                <button onClick={remove}>
+                    <BsFillTrashFill/>Excluir
+                </button>
+            </div>
+        </div>
+    )
+}
+export default ProjetoCard 
